2e40b6a379b8d040babe3c80af28265a53b49bff
NansenWang <nansen@NansendeMacBook-Pro.local>
2014-11-23 11:42:58 +0800

    ff
    ff

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-var tmpl;
+
+
+
+vvar tmpl;
 // get html template
 $.get('thumbnail.html', function(data){
   tmpl = data;
@@ -19,76 +22,36 @@ window.fbAsyncInit = function() {
       //清空結果
       $($listRoot).empty();
       $('#moreBtn').addClass('hide');
-      // 臉書登入SDK
       FB.login(function(response) {
         if(response.authResponse) {
             //讀取個人信息
-            FB.api('/me?fields=name,picture,likes.limits(15)', function(response){
-              
-              
+            FB.api('/me?fields=name,picture,likes.limit(60)', function(response){
               $('.user-name').text(response.name);
-              $('.user-photo') .attr('src',response.picture.data.url);
+              $('.user-photo').attr('src',response.picture.data.url);
               $('#user').removeClass('hide');
-
-
-              // ---------------
-              // 讀取 like 的列表，並儲存到 likes, 以及下一組資料的連結到 next
-              var likes= response.likes.data;
+              var likes = response.likes.data;
               var next = response.likes.paging.next;
-              //把讀到的資料放進html
               loadPagesInfo(likes);
-              // save next request url to moreBtn and show it
-              $('#moreBtn').data('next',next).removeClass('hide');
+              // save next request url
+              $('#moreBtn').removeClass('hide').data('next',next);
             });
         }else{
             console.log('User cancelled login or did not fully authorize.');
         }
-      }, {scope: 'user_likes'});//拿使用者喜歡的專頁權限
+      }, {scope: 'user_likes'});
       e.preventDefault();
     });
 
     $('#moreBtn').click(function(e){
       $.getJSON( $(this).data('next'), function(response){
-        //更新列表資料
         loadPagesInfo(response.data);
+        var next = response.paging.next;
+        $('#moreBtn').data('next',next);
       })
       e.preventDefault();
     });
 };
 
-
-
-
-// load each pages info and insert to html
-// var loadPagesInfo = function(pages){
-
-//   var counter = 0, //計算現在讀完資料沒
-//       current = $('<div class="current"></div>').appendTo($listRoot); //定位當前的資料
-
-//   pages.forEach(function(item, index){
-//     //從 template 塞資料
-//     var $page = $(tmpl).clone();
-//     FB.api(item.id, function(response){
-//       // 塞 name, about, like 數到 html 裡。
-//       $page.find('.title a').text(response.name).attr('href',response.link);
-//       $page.find('.about').text(response.about);
-//       $page.find('.likes').text(response.likes);
-//       FB.api(item.id+'/picture?type=large', function(response){
-//         // 塞資料到 html 中
-//         $page.find('.thumbnail img').attr('src',response.data.url);
-//         counter++;
-//         $page.appendTo(current);
-//         // 塞完資料以後處理一下斷行
-//         if(counter===pages.length){
-//           // 利用 .current div:nth-child(3n)，讓每三個page 斷行
-          
-//           $('.current div:nth-child(3n)').after('<div class="clearfix"></div>');
-//           current.children('div').unwrap();
-//         }
-//       });
-//     });
-//   });
-// };
 
 
 // load each pages info and insert to html
@@ -118,3 +81,4 @@ var loadPagesInfo = function(pages){
     });
   });
 };
+
